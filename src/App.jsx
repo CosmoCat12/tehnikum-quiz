e446394896ef624b906b81b6e8227987155e29e7
Samidjon <suleyman2.sn@gmail.com>
2024-07-29 20:25:06 +0300

    quiz_app

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Welcome from "./pages/Welcome";
+import StepOne from "./pages/StepOne";
+import StepTwo from "./pages/StepTwo";
+import StepThree from "./pages/StepThree";
+import StepFour from "./pages/StepFour";
+import Thanks from "./pages/Thanks";
+import "./styles/main.css";
+const App = () => {
+  return (
+    <div className="App">
+      <Welcome />
+    </div>
+  );
+};
+
+export default App;
